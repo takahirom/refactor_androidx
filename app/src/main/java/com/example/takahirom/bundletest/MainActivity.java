813bef028713c1753e77e053ae9e79a76bb44691
takahirom <takam.dev@gmail.com>
2018-05-10 00:31:15 +0900

    Refactor AndroidX

--- app/src/main/java/com/example/takahirom/bundletest/MainActivity.java
+++ app/src/main/java/com/example/takahirom/bundletest/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.takahirom.bundletest;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    com.google.android.material.floatingactionbutton.FloatingActionButton
+        fab = (com.google.android.material.floatingactionbutton.FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view) {
         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
